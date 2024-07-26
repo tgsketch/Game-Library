3669a2ed5f042af995a83f1ea7949a424cc373fc
tgsketch <hello@alexandernicholas.me>
2024-07-26 08:39:00 -0500

    Start Building Components - Conditional Rendering

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+function ListGroup() {
+  const items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
